ff3a1e3384dcc8bd3c654e61d897e82d7e5815c9
jwb <779510476@qq.com>
2021-04-06 14:13:28 +0800

    4.6星期二测试

--- runAllCases.py
+++ runAllCases.py
@@ -6,13 +6,10 @@ import unittest
 from HTMLTestRunner import HTMLTestRunner
 import time
 from common import config,new_report,send_mail
-
-
 def add_case(test_path=config.TEST_CASE):
     """加载所有的测试用例"""
     discover = unittest.defaultTestLoader.discover(test_path, pattern='*API.py')
     return discover
-
 def run_case(all_case,result_path=config.TEST_REPORT):
     """执行所有的测试用例"""
     # 初始化接口测试数据
