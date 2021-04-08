a61b2daad42c241d2e3d6ec75a2fadeadd927716
JWB <779510476@qq.com>
2021-04-08 17:48:01 +0800

    4.8测试

--- common/ceshi.py
+++ common/ceshi.py
@@ -1,4 +1,4 @@
 from common import config,new_report,send_mail
-aa = new_report.new_report(config.TEST_REPORT)
-print(aa.replace("\\", "/"))
-send_mail.SEND_MAIL().send_mail('2514095967@qq.com',report=aa.replace("\\", "/"))
+# aa = new_report.new_report(config.TEST_REPORT)
+# print(aa.replace("\\", "/"))
+# send_mail.SEND_MAIL().send_mail('2514095967@qq.com',report=aa.replace("\\", "/"))
